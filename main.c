cf4aab9778a307cffa48e0cbca414f630d1b55e2
Jonah Williams <jonah.williams24@icloud.com>
2019-04-20 02:13:02 +0000

    edit decryption of sub cipher (rn its same as encryption)

--- main.c
+++ main.c
@@ -31,19 +31,19 @@ int main() {
     char message[n]; 
     char sub[n];
     printf("\nWelcome to the cipher Code Program!\n"); //This prints the title card
-    printf("Select the required function by inputting the relating operator into the 'input' file:\n\n"); 
+    printf("Select the required function by typing the relating operator into 'input' on the first line:\n\n"); 
     //this prompts the user to type the operator required so they can choose the function they want
-    printf("To encrypt a message using a rotational cipher, with a desired key, input '0' into the 'input' file.\n"); 
+    printf("To encrypt a message using a rotational cipher, with a desired key, type '0' into the 'input' file.\n"); 
     //option 1 is for the user to input a message and a key for encryption with the rotational cipher
-    printf("To decrypt a message with a rotational cipher with a known key, input '1' into the 'input' file.\n");
+    printf("To decrypt a message with a rotational cipher with a known key, type '1' into the 'input' file.\n");
     //option 2 is to decrypt a message when the user knows the key of a rotation cipher message 
-    printf("To encrypt a message using a desired substitution, input '2' into the 'input' file.\n");
+    printf("To encrypt a message using a desired substitution, type '2' into the 'input' file.\n");
     //option 3 is for the user to encrpt using a substitution where they can choose which letters are changed to what
-    printf("To decrypt a message with a rotational cipher without knowing a key, input '3' into the 'input' file.\n");
+    printf("To decrypt a message with a substitution cipher knowing the substitution used, type '3' into the 'input' file.\n");
     //this is where the program will attempt to decipher the message of a rotational cipher without knowing the key
-    printf("To decrpyt a message with a substition cipher knowing the substitution used, input '4' into the 'input' file.\n");
+    printf("To decrpyt a message with a substition cipher knowing the substitution used, type '4' into the 'input' file.\n");
     //This will decrypt a message using the users substitution index
-    printf("To decrypt a substitution cipher without knowing the substition input '5' into the input file.\n");
+    printf("To decrypt a substitution cipher without knowing the substition type '5' into the input file.\n");
     //This is the hardest one to do with 26! possible combinations. BRUTE FORCE WILL NOT WORK!!!
     //printf("Operator: \n");
     scanf("%d", &operator); //This scans the input from the user
@@ -55,13 +55,7 @@ int main() {
             printf("\nERROR INVALID OPERATOR\n"); // Any other input prints the error message...
             exit(0); // ...and exits the program
         }
-        /*if (operator == 0 || operator == 1) // this is for using the rotation cipher function
-        { 
-            //printf("Enter message: \n"); // the message is the one that needs to be encrypted or decyrpted 
-            scanf(" %[^\n]s", message); // scans the message written until a new line
-            printf("Message is: %s\n", message); // this prints the message itself
-            //this also works to check for any issue in scanning the message by the user (manually)
-         }*/
+       
         switch(operator) {
             case 0:
                 scanf(" %[^\n]s", message); // scans the message written until a new line
@@ -127,6 +121,8 @@ int main() {
 
                 break;
             case 2:
+                printf("\nEnter message to be encrypted onto line 2 of 'input'.\n");
+                printf("Enter substitution key in alphabetical order with no spaces onto line 3 of 'input'.");
                 scanf(" %[^\n]s", message);
                 scanf("%s", sub);
                 printf("\nSubstitution in alphabetical order is: ");
@@ -251,20 +247,137 @@ int main() {
                     default:
                     printf("%c", message[i]);                    
                     }
-            }
-
-
-                
-
-                    
-                    // first convert sub to caps, then write what each letter becomes
-                    //find 'A' use a loop to print a-z and sub next to it
-                    //convert letters in message to letters from sub
-                
-                
-
+            }             
             exit(0);
             case 3:
+                printf("\nEnter message to be decrypted onto line 2 of 'input'.\n");
+                printf("Enter substitution key in alphabetical order with no spaces onto line 3 of 'input'.");
+                scanf(" %[^\n]s", message);
+                scanf("%s", sub);
+                printf("\nSubstitution in alphabetical order is: ");
+                for(int i=0; sub[i] != '\0'; ++i){
+                    if(sub[i] <=122 && sub[i] >= 97){
+                        sub[i] = sub[i] - 32;
+                    }
+                }
+                printf("%s\n", sub);
+                printf("Original message is:\n%s", message);
+                printf("Decrypted message is: \n");
+                for(int i=0; message[i] != '\0' && i <= 200; ++i ){
+                    if (message[i] <=122 && message[i] >=97){
+                            message[i] = message[i] - 32;
+                    }
+                    
+                    switch(message[i]){
+                    case 65:
+                    message[i]=sub[0];
+                    printf("%c", message[i]);
+                    break;
+                    case 66:
+                    message[i]=sub[1];
+                    printf("%c", message[i]);
+                    break;
+                    case 67:
+                    message[i]=sub[2];
+                    printf("%c", message[i]);
+                    break;
+                    case 68:
+                    message[i]=sub[3];
+                    printf("%c", message[i]);
+                    break;
+                    case 69:
+                    message[i]=sub[4];
+                    printf("%c", message[i]);
+                    break;                    
+                    case 70:
+                    message[i]=sub[5];
+                    printf("%c", message[i]);
+                    break;
+                    case 71:
+                    message[i]=sub[6];
+                    printf("%c", message[i]);
+                    break;                    
+                    case 72:
+                    message[i]=sub[7];
+                    printf("%c", message[i]);
+                    break;                    
+                    case 73:
+                    message[i]=sub[8];
+                    printf("%c", message[i]);
+                    break;                    
+                    case 74:
+                    message[i]=sub[9];
+                    printf("%c", message[i]);
+                    break;                    
+                    case 75:
+                    message[i]=sub[10];
+                    printf("%c", message[i]);
+                    break;                    
+                    case 76:
+                    message[i]=sub[11];
+                    printf("%c", message[i]);
+                    break;                    
+                    case 77:
+                    message[i]=sub[12];
+                    printf("%c", message[i]);
+                    break;                    
+                    case 78:
+                    message[i]=sub[13];
+                    printf("%c", message[i]);
+                    break;                    
+                    case 79:
+                    message[i]=sub[14];
+                    printf("%c", message[i]);
+                    break;                    
+                    case 80:
+                    message[i]=sub[15];
+                    printf("%c", message[i]);
+                    break;                    
+                    case 81:
+                    message[i]=sub[16];
+                    printf("%c", message[i]);
+                    break;
+                    case 82:
+                    message[i]=sub[17];
+                    printf("%c", message[i]);
+                    break;
+                    case 83:
+                    message[i]=sub[18];
+                    printf("%c", message[i]);
+                    break;
+                    case 84:
+                    message[i]=sub[19];
+                    printf("%c", message[i]);
+                    break;
+                    case 85:
+                    message[i]=sub[20];
+                    printf("%c", message[i]);
+                    break;
+                    case 86:
+                    message[i]=sub[21];
+                    printf("%c", message[i]);
+                    break;
+                    case 87:
+                    message[i]=sub[22];
+                    printf("%c", message[i]);
+                    break;                 
+                    case 88:
+                    message[i]=sub[23];
+                    printf("%c", message[i]);
+                    break;
+                    case 89:
+                    message[i]=sub[24];
+                    printf("%c", message[i]);
+                    break;
+                    case 90:
+                    message[i]=sub[25];
+                    printf("%c", message[i]);
+                    break;
+                    default:
+                    printf("%c", message[i]);                    
+                    }
+            }             
+                
             exit(0);
             case 4:
             exit(0);
